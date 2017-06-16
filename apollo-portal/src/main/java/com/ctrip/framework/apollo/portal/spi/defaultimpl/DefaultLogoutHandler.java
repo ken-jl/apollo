02f24564443703fb45520480cae32dffbff95917
Ternence <mjhx_zt@qq.com>
2017-06-16 18:21:24 +0800

    BUG修复：解决退出后重新登录找不到地址的问题

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/spi/defaultimpl/DefaultLogoutHandler.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/spi/defaultimpl/DefaultLogoutHandler.java
@@ -1,17 +1,18 @@
 package com.ctrip.framework.apollo.portal.spi.defaultimpl;
 
-import com.ctrip.framework.apollo.portal.spi.LogoutHandler;
-
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ctrip.framework.apollo.portal.spi.LogoutHandler;
 
 public class DefaultLogoutHandler implements LogoutHandler {
 
   @Override
   public void logout(HttpServletRequest request, HttpServletResponse response) {
     try {
+      request.getSession().invalidate(); // 过期session
       response.sendRedirect("/");
     } catch (IOException e) {
       throw new RuntimeException(e);
